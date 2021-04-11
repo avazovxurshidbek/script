b365b99832e665920a1f92180e5898798da8389f
--global <--global>
2021-04-11 15:09:05 +0500

    versiya 1.0

---
+++ script.js
@@ -0,0 +1,22 @@
+var ism = prompt('Ismingizni kiriting?')
+console.log('Mening ismim', ism)
+
+var yosh = prompt('Yoshingizni kiriting')
+console.log('Men', yosh, 'yoshdaman')
+
+var misol = prompt('Misolni yeching 6 + 3 =')
+console.log('Misol 1: 6 + 3 = 9 Sizning javob', misol)
+
+var misol = prompt('Misolni yeching 20 - 5 =')
+console.log('Misol 2: 20 - 5 = 15 Sizning javob', misol)
+
+var misol = prompt('Misolni yeching 15 * 2 =')
+console.log('Misol 3: 15 * 2 = 30 Sizning javob', misol)
+
+var misol = prompt('Misolni yeching 8 / 4 =')
+console.log('Misol 4: 8 / 4 = 2 Sizning javob', misol)
+
+var misol = prompt('Misolni yeching  10 % 3 =')
+console.log('Misol 1: 10 % 3 = 1 Sizning javob', misol)
+
+alert('Natijani korish uchun consoleni oching')
