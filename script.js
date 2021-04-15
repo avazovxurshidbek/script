36c9c10101ce353ecbeff1f8f38d51c5ecb0aef8
--global <--global>
2021-04-15 05:20:52 +0500

    2-dars

--- script.js
+++ script.js
@@ -1,22 +1,75 @@
-var ism = prompt('Ismingizni kiriting?')
-console.log('Mening ismim', ism)
 
-var yosh = prompt('Yoshingizni kiriting')
-console.log('Men', yosh, 'yoshdaman')
 
-var misol = prompt('Misolni yeching 6 + 3 =')
-console.log('Misol 1: 6 + 3 = 9 Sizning javob', misol)
 
-var misol = prompt('Misolni yeching 20 - 5 =')
-console.log('Misol 2: 20 - 5 = 15 Sizning javob', misol)
+// 1-homework
 
-var misol = prompt('Misolni yeching 15 * 2 =')
-console.log('Misol 3: 15 * 2 = 30 Sizning javob', misol)
+// var ism = prompt('Ismingizni kiriting?')
+// if(ism == 'Xurshidbek'){
+//     alert('Hello Boss')   
+// }
+// else{
+//    alert('Hello guest')   
+// }
 
-var misol = prompt('Misolni yeching 8 / 4 =')
-console.log('Misol 4: 8 / 4 = 2 Sizning javob', misol)
+// 2-homework
 
-var misol = prompt('Misolni yeching  10 % 3 =')
-console.log('Misol 1: 10 % 3 = 1 Sizning javob', misol)
+// var  yosh = prompt('Yoshingizni kiriting')
+// if( yosh <= 0){
+//     alert("Nimadir notog'ri ketib qoldi")   
+// }
+// else if(0 <= yosh,yosh <= 18){
+//     alert("Yoshsiz. O'qishingiz kerak")   
+// }
+// else if(   18 < yosh, yosh <= 50){
+//     alert("Ishlashingiz kerak")   
+// }
+// else if(   50 < yosh, yosh <= 59){
+//     alert("Yaqinda pensiyaga chiqasiz")   
+// }
+// else if(   59 < yosh, yosh <= 150){
+//     alert("Pensiyanerga o'xshaysiz, hali tirik bo'lsangiz")   
+// }
+// else  {
+//    alert("Nimadir notog'ri ketib qoldi")   
+// }
 
-alert('Natijani korish uchun consoleni oching')
+// 3-homework
+
+var a = +prompt("1-soni kiriting") 
+var b = +prompt("2-soni kiriting") 
+var c = +prompt("3-soni kiriting") 
+    
+if (b  > a && a > c ) {
+    alert("O'rta qiymat "+a) 
+}     
+else  if (b  < a && a < c ){
+    alert("O'rta qiymat "+a) 
+}
+else  if (a > b && b > c ) {
+    alert("O'rta qiymat "+b) 
+  }    
+else  if ( a < b && b < c ) {
+    alert("O'rta qiymat "+b) 
+  }    
+else if (b > c && c > a) {
+    alert("O'rta qiymat "+c) 
+}
+else if ( b < c && c < a) {
+    alert("O'rta qiymat "+c) 
+}
+
+// 4-homework
+
+
+// var son=+prompt("Son kiriting")
+// k=0
+// while(k <= son){
+// c=k
+// console.log(c);
+// k=c+1
+// }
+// alert("Rasa assabni buzadidan narsa ekan tortinchisi");
+
+
+
+
